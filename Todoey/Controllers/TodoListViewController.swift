0bc3f2b07239bc11063b85be5ca0717c73a4f575
inatalia <inat_7@yahoo.com>
2019-05-01 22:21:25 -0700

    NSCoder

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -11,23 +11,14 @@ import UIKit
 class TodoListViewController: UITableViewController {
 
     var itemArray = [Item]()
-    let defaults = UserDefaults.standard
+    
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        let newItem = Item()
-        newItem.title = "itemArray"
-        itemArray.append(newItem)
-        let newItem2 = Item()
-        newItem2.title = "itemArray"
-        itemArray.append(newItem2)
-        let newItem3 = Item()
-        newItem3.title = "itemArray"
-        itemArray.append(newItem3)
-        let newItem4 = Item()
-        newItem4.title = "itemArray"
-        itemArray.append(newItem4)
+        
+        loadItems()
     }
     
     //MARK - TableView Datasource Methods
@@ -52,8 +43,9 @@ class TodoListViewController: UITableViewController {
     //MARK - TableView Delegate Methods
     /* Select a cell with animated and Add checkmarks */
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
         itemArray[indexPath.row].done = !itemArray[indexPath.row].done
-        tableView.reloadData()
+        saveItems()
         tableView.deselectRow(at: indexPath, animated: true)
         
     }
@@ -67,9 +59,10 @@ class TodoListViewController: UITableViewController {
             //Something happens once user clicks the add item button on UIAlert
             let newItem = Item()
             newItem.title = textField.text!
+            
             self.itemArray.append(newItem)
-            self.defaults.set(self.itemArray, forKey: "TodoListArray")
-            self.tableView.reloadData()
+           
+            self.saveItems()
         }
         
         //Text field place holder
@@ -83,6 +76,27 @@ class TodoListViewController: UITableViewController {
         present(alert, animated: true, completion: nil)
     }
     
+    // MARK - Model Manupulation Methods
+    func saveItems() {
+        let encoder = PropertyListEncoder()
+        do {
+            let data = try encoder.encode(itemArray)
+            try data.write(to : dataFilePath!)
+        } catch {
+            print("Error encoding item array \(error)")
+        }
+        tableView.reloadData()
+    }
     
+    func loadItems() {
+       if let data = try? Data(contentsOf: dataFilePath!) {
+            let decoder = PropertyListDecoder()
+            do {
+                itemArray = try decoder.decode([Item].self, from: data)
+            } catch {
+                print("Error decoding item array \(error)")
+            }
+        }
+    }
 }
 
